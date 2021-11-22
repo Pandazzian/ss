aed3065f12722567710b3148310397f228c276ce
gemthegang <n.pongsawut@thegang.tech>
2021-11-22 18:23:10 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div style="background-image: url('/assets/img/bg.png')"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'main_overlay',
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
